5a3f5a466ab48cd0c404ee56ca713e7c04a0ec12
ShardaGhodki <98203186+ShardaGhodki@users.noreply.github.com>
2024-06-12 19:24:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
-import logging
-import random
+
 
 print ("hello");
 
 
-def isEven():
+hellodef isEven():
     $_current_number = random.randint(1,100)
     print (current_number)
     if current_number % 2 != 0:
         return "odd"
     else:
-        return "even"
+        return "even
 
 wasiteven = iEven()
 
